e8470d4cfccf8175ebbda7eab02519ddc171ffcf
Rob Shiels <rob@robshiels.com>
2026-01-11 18:33:46 +0000

    Creating the Stories app and templates

--- fanfic_site/stories/views.py
+++ fanfic_site/stories/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Steve K',
+        'title': 'First Blog Post',
+        'content': 'Content for the first post.',
+        'date_posted': '12/07/2023',
+    },
+    {
+        'author': 'Scott K',
+        'title': 'Second Blog Post',
+        'content': 'Content for the second post.',
+        'date_posted': '13/07/2023',
+    }
+]
+
 def home(request):
-    return HttpResponse('<h1>Stories Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'stories/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Stories-About</h1>')
+    return render(request, 'stories/about.html', {'title': 'About'})
